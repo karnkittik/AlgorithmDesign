8ca90e35bdcee1ccd67922b336dbad72b5c36dc6
Karnkitti Kittikamron <karnkitti_tang@hotmail.com>
2019-01-09 23:36:24 +0700

    all

--- PairSum.cpp
+++ PairSum.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
+#include <set>
+
 using namespace std;
-
-template <class T>
-vector<T> input_split() {
-    cin >> ws;
-    string line;
-    getline(cin, line);
-    stringstream ss(line);
-    T buf;
-    vector<T> tokens;
-    while (ss >> buf)
-        tokens.push_back(buf);
-    return tokens;
-}
 int main(){
+    int a,b;
+    cin >> a >> b;
+    vector<int> c;
+    set<int> d;    
+    int u;
+    for(int i=0;i<a;i++){
+        cin >> u;
+        d.insert(u);
+    }
 
-    
+    for(int i=0;i<b;i++){
+        cin >> u;
+        c.push_back(u);
+    }
+    for(auto v:c){
+        string out = "NO";
+       for(auto u:d) {
+           if(d.count(v-u)>0){
+               out = "YES"; 
+               break;
+           }
+        } 
+        cout << out <<endl;
+    }
+    return 0;
 }
