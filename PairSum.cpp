c83cf666d424187ff292f91ebca29aebc3d58966
Karnkitti Kittikamron <karnkitti_tang@hotmail.com>
2019-01-09 22:49:15 +0700

    inputsplit

--- PairSum.cpp
+++ PairSum.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
 using namespace std;
 
+template <class T>
+vector<T> input_split() {
+    cin >> ws;
+    string line;
+    getline(cin, line);
+    stringstream ss(line);
+    T buf;
+    vector<T> tokens;
+    while (ss >> buf)
+        tokens.push_back(buf);
+    return tokens;
+}
+int main(){
+
+    
+}
